fcc67f144b96f24c1e26677c9bcdcc70d478beff
Diego Chavez <defraindefrain.chavez@alumnos.udg.mx>
2026-05-12 23:26:01 -0600

    Menu y navegacion

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -3,19 +3,18 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
+
     public Vector3 offset;
 
     void LateUpdate()
     {
         if (player != null)
         {
-            Vector3 newPosition = new Vector3(
-                player.position.x,
+            transform.position = new Vector3(
+                player.position.x + offset.x,
                 offset.y,
                 player.position.z + offset.z
             );
-
-            transform.position = newPosition;
         }
     }
 }
